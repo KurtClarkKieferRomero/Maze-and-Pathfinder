1b124bbb3ea1d9bdb606393b0a48984d8388a0df
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2023-02-13 14:59:44 +0800

    Added neighbor cell checker method

--- Maze and Pathfinder.py
+++ Maze and Pathfinder.py
@@ -39,6 +39,21 @@ class Cell:
             return False
         return grid_cells[find_index(x, y)]
 
+    def check_neighbors(self):
+        neighbors = []
+        top = self.check_cell(self.x, self.y - 1)
+        right = self.check_cell(self.x + 1, self.y)
+        bottom = self.check_cell(self.x, self.y + 1)
+        left = self.check_cell(self.x - 1, self.y)
+        if top and not top.visited:
+            neighbors.append(top)
+        if right and not right.visited:
+            neighbors.append(right)
+        if bottom and not bottom.visited:
+            neighbors.append(bottom)
+        if left and not left.visited:
+            neighbors.append(left)
+        return choice(neighbors) if neighbors else False
 
 grid_cells = [Cell(col, row) for row in range(rows) for col in range(cols)]
 current_cell = grid_cells[0]
