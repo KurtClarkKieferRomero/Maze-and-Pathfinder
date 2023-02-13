44ffce3f972aecfcf8a0213360d251d97df0d906
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2023-02-13 16:33:12 +0800

    Added imports for later use

--- Pathfinder.py
+++ Pathfinder.py
@@ -1,4 +1,6 @@
 import pygame as pg
+from random import random
+from collections import deque
 
 cols, rows = 25, 15
 TILE = 60
@@ -9,3 +11,7 @@ clock = pg.time.Clock()
 
 while True:
     sc.fill(pg.Color('black'))
+
+    [exit() for event in pg.event.get() if event.type == pg.QUIT]
+    pg.display.flip()
+    clock.tick(10)
