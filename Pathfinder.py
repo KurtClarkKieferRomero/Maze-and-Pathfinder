ce760e0d25492625a4cfcca2700ed0b9b6c60ceb
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2023-02-18 18:09:58 +0800

    dictionary of adjacency lists

--- Pathfinder.py
+++ Pathfinder.py
@@ -18,12 +18,17 @@ sc = pg.display.set_mode([cols * TILE, rows * TILE])
 clock = pg.time.Clock()
 grid = [[1 if random() < 0.2 else 0 for col in range(cols)]
         for row in range(rows)]
-grap = {}
+graph = {}
+for y, row in enumerate(grid):
+    for x, col in enumerate(row):
+        if not col:
+            graph[(x, y)] = graph.get((x, y), []) + get_next_nodes(x, y)
 
 while True:
     sc.fill(pg.Color('black'))
     # draw grid
-    [[pg.draw.rect(sc, pg.Color('darkorange'), get_rect(x, y), border_radius=TILE // 5) for x, col in enumerate(row) if col] for y, row in enumerate(grid)]
+    [[pg.draw.rect(sc, pg.Color('darkorange'), get_rect(x, y), border_radius=TILE // 5) for x, col in enumerate(row)
+      if col] for y, row in enumerate(grid)]
 
     [exit() for event in pg.event.get() if event.type == pg.QUIT]
     pg.display.flip()
