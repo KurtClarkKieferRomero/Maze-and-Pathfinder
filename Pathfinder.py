b99f83c45154cf5dd6f351df00716dec9c4631b1
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2023-02-18 17:33:45 +0800

    Added Node checker method

--- Pathfinder.py
+++ Pathfinder.py
@@ -5,6 +5,10 @@ from collections import deque
 def get_rect(x, y):
     return x * TILE + 1, y * TILE + 1, TILE - 2, TILE - 2
 
+def get_next_nodes(x, y):
+    check_next_node = lambda x, y: True if 0 <= x < cols and 0 <= y < rows and not grid[y][x] else False
+    ways = [-1, 0], [0, -1], [1, 0], [0, 1]
+    return[(x + dx, y + dy) for dx, dy in ways if check_next_node(x + dx, y + dy)]
 
 cols, rows = 25, 15
 TILE = 60
@@ -14,6 +18,7 @@ sc = pg.display.set_mode([cols * TILE, rows * TILE])
 clock = pg.time.Clock()
 grid = [[1 if random() < 0.2 else 0 for col in range(cols)]
         for row in range(rows)]
+grap = {}
 
 while True:
     sc.fill(pg.Color('black'))
